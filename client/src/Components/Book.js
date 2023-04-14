c40f5bb7bd9d2a8b1813c306273482044742c1a1
Baskakovs <baskakovs@me.com>
2023-04-15 00:45:37 +0300

    New review form keeps track of inputs

--- client/src/Components/Book.js
+++ client/src/Components/Book.js
@@ -5,7 +5,7 @@ import {AppContext} from '../App'
 import ReviewCard from './ReviewCard';
 function Book(){
     const params = useParams()
-    const {data} = useContext(AppContext)
+    const {data, currentUser} = useContext(AppContext)
     const [book, setBook] = useState("")
     useEffect(() => {
         if(Array.isArray(data)){
@@ -14,6 +14,32 @@ function Book(){
             setBook(book)
         }
     }, [data])
+
+    //HANDLING NEW REVIEW
+    //===================
+    const [newReview, setNewReview] = useState(null)
+    useEffect(() => {
+        if(currentUser){
+            const review = {
+                title: "",
+                content: "",
+                user_id: currentUser.id,
+                book_id: book.id
+            }
+            setNewReview(review)
+        }else{
+            return null
+        }
+    }, [currentUser, book])
+    
+    function handleNewReview(e){
+        const {name, value} = e.target
+        setNewReview({
+            ...newReview, [name]: value
+        })
+    }
+
+
         
     return(
         <>
@@ -22,18 +48,22 @@ function Book(){
             book.reviews.map((review) =>
             <ReviewCard review={review} key={review.id}/>)
             }
-            <div className='container one-col mt-7'>
-                <input type="text" 
-                placeholder="Title" 
-                className="no-border text-bold text-large"
-                autoFocus
-                />
-                <textarea 
-                type="text" 
-                placeholder="What did you think of this book?" 
-                className="new-review-input"/>
-                <button className="btn-purple mt-7">Add Review</button>
-            </div>
+            {
+                !currentUser ? null :
+                <form className='container one-col mt-7'>
+                    <input type="text" 
+                    placeholder="Title" 
+                    className="no-border text-bold text-large"
+                    autoFocus
+                    />
+                    <textarea 
+                    type="text" 
+                    placeholder="What did you think of this book?" 
+                    className="new-review-input"/>
+                    <button className="btn-purple mt-7">Add Review</button>
+                </form>
+            }
+
         </div>
 
         </>
